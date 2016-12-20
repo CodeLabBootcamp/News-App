eaeab863f18589e2edeb1dbdb5fd26cb1f527514
SniperDW <sabri@d3stud.io>
2016-12-20 18:33:20 +0300

    Added basic GET request

--- app/src/main/java/com/codelab/newsapp/MainActivity.java
+++ app/src/main/java/com/codelab/newsapp/MainActivity.java
@@ -3,12 +3,8 @@ package com.codelab.newsapp;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 import com.google.gson.Gson;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -17,19 +13,13 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Object request;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getRequest("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%3D2502265&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=");
 
-//        new RequestTask().execute("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%3D2502265&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=");
-    }
-
-    public void getRequest(String uri) {
+        new RequestTask().execute("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%3D2502265&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=");
     }
 
 
@@ -38,12 +28,39 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected String doInBackground(String... uri) {
 
-            for (long i = 0; i < Long.MAX_VALUE; i++) {
+            String jsonString = "";
+            try {
 
+                URL url = new URL(uri[0]);
+
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+                urlConnection.setRequestMethod("GET");
+
+                urlConnection.setReadTimeout(10000 /* milliseconds */);
+                urlConnection.setConnectTimeout(15000 /* milliseconds */);
+
+                urlConnection.setDoOutput(true);
+
+                urlConnection.connect();
+
+                BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+
+                char[] buffer = new char[1024];
+
+                StringBuilder sb = new StringBuilder();
+                String line;
+                while ((line = br.readLine()) != null) {
+                    sb.append(line + "\n");
+                }
+
+                jsonString = sb.toString();
+
+            } catch (Exception e) {
+                e.printStackTrace();
             }
 
-
-            return "";
+            return jsonString;
         }
 
         @Override
